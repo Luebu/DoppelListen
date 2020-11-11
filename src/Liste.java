8b096577840b8734fe37a5bea0533aae61aa51f3
Luebu <maximilian.luecker@hebk.de>
2020-11-11 11:12:05 +0100

    Initial commit

---
+++ src/Liste.java
@@ -0,0 +1,47 @@
+public class Liste {
+    //	remove first():Person
+    //		Löscht das erste Element und das 2te wird zu dem ersten Element
+    //		Das gelöschte Element wird zurückgegeben
+    public Person removeFirst(){
+    return null;
+    }
+
+
+    //	remove last():Person
+    //		Löscht das Letzte Element und das vorletzte wird zum letzten
+    //		Das gelöschte Element wird zurückgegeben
+    public Person removeLast(){
+        return null;
+    }
+
+
+    //	removeN(pN: int):Person
+    //		löscht das Element an der gegebenen Stelle und gibt das gelöschte Element zurück
+    public Person removeN (int pN){
+        return null;
+    }
+
+
+    //	removePerson(pPerson: Person):boolean
+    //		Durchsucht die Liste nach der gegebenen Person und löscht diese.
+    //		Bei erfolgreichem löschen wird true bei nicht erfolgreichem löschen durch z.B. Person nicht gefunden wird false zurückgegeben
+    public boolean removePerson(Person pPerson){
+        return true;
+    }
+
+
+    //	add(pPerson: Person): void
+    //		Fügt ein Element mit der gegebenen Person an der letzten Stelle hinzu
+    public void add(Person pPerson){
+
+    }
+
+
+    //	addN(pPerson: Person, pNumber: int): boolean
+    //		Fügt ein Element mit der gegebenen Person and der gegebenen Stelle hinzu.
+    //      Bei erfolgreichem hinzufügen wird true andernfalls false zurückgegeben
+    public boolean addN(Person pPerson, int pN){
+        return true;
+    }
+
+}
